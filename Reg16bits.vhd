43900e8958b4763025c1d5533da66dd2d711cb1c
AguiarEmerson <aguiaremer@gmail.com>
2024-11-11 22:26:02 -0300

    Consertei o Registrador

--- Reg16bits.vhd
+++ Reg16bits.vhd
@@ -2,21 +2,21 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity Reg19bits is
+entity Reg16bits is
    port( clk, wr_en, rst : in std_logic;
-         data_in  : in unsigned(18 downto 0);
-         data_out : out unsigned(18 downto 0)
+         data_in  : in unsigned(15 downto 0);
+         data_out : out unsigned(15 downto 0)
    );
-end Reg19bits;
+end Reg16bits;
 
-architecture a_Reg19bits of Reg19bits is
-    signal registro: unsigned(18 downto 0);
+architecture a_Reg16bits of Reg16bits is
+    signal registro: unsigned(15 downto 0);
 
 begin
     process(clk,rst,wr_en) 
     begin                
        if rst='1' then
-          registro <= "0000000000000000000";
+          registro <= "0000000000000000";
        elsif wr_en='1' then
           if rising_edge(clk) then
              registro <= data_in;
